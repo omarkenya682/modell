b22df7f0a3e6592438848c5db00b4e34e3c431de
omarkenya682 <marriaqueen0202@gmail.com>
2026-01-26 09:17:24 +0300

    Update projects.ts

--- data/projects.ts
+++ data/projects.ts
@@ -1,1 +1,13 @@
+import type { Property } from '../types';
+
+// Import all project JSON files at build time
+const modules = import.meta.glob('./projects/*.json', {
+  eager: true,
+});
+
+const projects: Property[] = Object.values(modules).map(
+  (mod: any) => mod.default
+);
+
+export default projects;
 
